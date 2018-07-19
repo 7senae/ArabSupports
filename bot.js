ce26c363a4eec0d806fb4f25a95613a4686088b7
7sena <40254036+7sena@users.noreply.github.com>
2018-07-19 10:55:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Bot By TEAM ArabSupport ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` Bot By KillerFox | $help `,"http://twitch.tv/S-F")
+client.user.setGame(` Bot By TEAM ArabSupport `,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
@@ -35,48 +35,12 @@ if(msg.content.startsWith ('^server')) {
   });
 
 
-
-    client.on('message', msg => { 
-      if (msg.content.startsWith(`^sug`)) {
-         let args = msg.content.split(" ").slice(1);
-        if (!args[1]) return msg.reply(`يجب كتابه الاقتراح`)
-        if (msg.guild.channels.find('name', 'suggest')) {
-          msg.guild.channels.find('name', 'suggest').send(`
-        الاقتراح من : ${msg.member}
-        الاقتراح : **${args.join(" ").split(msg.mentions.members.first()).slice(' ')}**
-        `)
-        }
-      }
-      })
-
-
-client.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find('name', 'welcome');
-    let memberavatar = member.user.avatarURL
-      if (!channel) return;
-    let embed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(memberavatar)
-        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
-        .addField(':loudspeaker: | اطلق من دخل' , `Welcome to the server, ${member}`)
-        .addField(':id: | user :', "**[" + `${member.id}` + "]**" )
-                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
-               
-                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
-                     
-                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
-                                       
-     .setFooter(`${member.guild.name}`)
-        .setTimestamp()
-   
-      channel.sendEmbed(embed);
-    });
   client.on("message", message => {
  if (message.content === "^help-js-bc") {
   const embed = new Discord.RichEmbed()
       .setColor("RANDOM")
       .setFooter('─═════════ {✯FoxBot✯} ════════════─')
-	  .addField('**$help-js-bc-1 ➺',` ⦁ جميع البرودكاست  ⦁**`)
+	  .addField('**^help-js-bc-1 ➺',` ⦁ جميع البرودكاست  ⦁**`)
       .setFooter('─═════════ {✯FoxBot✯} ════════════─')
   message.author.send({embed});
   }
@@ -98,7 +62,7 @@ client.on('guildMemberAdd', member => {
 
 
 client.on("message", message => {
- if (message.content === "$help-js-welcome-1") {
+ if (message.content === "^help-js-welcome-1") {
   const embed = new Discord.RichEmbed()
       .setColor("RANDOM")
       .setFooter('─═════════ {✯ArabSupport✯} ════════════─')
@@ -111,156 +75,17 @@ client.on("message", message => {
 });
 
 client.on("message", message => {
- if (message.content === "$help-js-welcome") {
+ if (message.content === "^help-js-welcome") {
   const embed = new Discord.RichEmbed()
       .setColor("RANDOM")
       .setFooter('─═════════ {✯ArabSupport✯} ════════════─')
-      .addField('**$help-js-welcome-1➺',` ⦁جميع اكواد الترحيب⦁**`)
+      .addField('**^help-js-welcome-1➺',` ⦁جميع اكواد الترحيب⦁**`)
       .setFooter('─═════════ {✯ArabSupport✯} ════════════─')
   message.author.send({embed});
   }
 });
 
-    
-    client.on('guildMemberRemove', member => {
-        var embed = new Discord.RichEmbed()
-        .setAuthor(member.user.username, member.user.avatarURL)
-        .setThumbnail(member.user.avatarURL)
-        .setTitle(`بس بعرف وين رحت؟؟؟ :raised_hand::skin-tone-1: :pensive:`)
-        .setDescription(`مع السلامه تشرفنا بك :raised_hand::skin-tone-1: :pensive: `)
-        .addField(':bust_in_silhouette:   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
-        .setColor('RED')
-        .setFooter(`====ولكم منور السيرفر اتمنا لك الاستمتاع====`, 'https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
-    
-    var channel =member.guild.channels.find('name', 'welcome')
-    if (!channel) return;
-    channel.send({embed : embed});
-    })
 
-  
-  var Himo_04 = ["https://f.top4top.net/p_682it2tg6.png","https://e.top4top.net/p_682a1cus5.png","https://d.top4top.net/p_682pycol4.png","https://c.top4top.net/p_682vqehy3.png","https://b.top4top.net/p_682mlf9d2.png","https://a.top4top.net/p_6827dule1.png","https://b.top4top.net/p_682g1meb10.png","https://a.top4top.net/p_682jgp4v9.png","https://f.top4top.net/p_682d4joq8.png","https://e.top4top.net/p_6828o0e47.png","https://d.top4top.net/p_6824x7sy6.png","https://c.top4top.net/p_682gzo2l5.png","https://b.top4top.net/p_68295qg04.png","https://a.top4top.net/p_682zrz6h3.png","https://f.top4top.net/p_6828vkzc2.png","https://e.top4top.net/p_682i8tb11.png","https://f.top4top.net/p_8816hvic1.png","https://d.top4top.net/p_882020461.png","https://e.top4top.net/p_882s3ftn1.png","https://a.top4top.net/p_882eg9c51.png","https://c.top4top.net/p_882xkcqd1.png","https://f.top4top.net/p_882w7pdi1.png","https://a.top4top.net/p_882gcpmo1.png"]
-    client.on('message', message => {
-        var args = message.content.split(" ").slice(1);
-    if(message.content.startsWith('^لو خيروك')) {
-         var cat = new Discord.RichEmbed()
-.setImage(Himo_04[Math.floor(Math.random() * Himo_04.length)])
-message.channel.sendEmbed(cat);
-    }
-});
-  
-  
-  const Sra7a = [
-    'صراحه  |  صوتك حلوة؟',
-    'صراحه  |  التقيت الناس مع وجوهين؟',
-    'صراحه  |  شيء وكنت تحقق اللسان؟',
-    'صراحه  |  أنا شخص ضعيف عندما؟',
-    'صراحه  |  هل ترغب في إظهار حبك ومرفق لشخص أو رؤية هذا الضعف؟',
-    'صراحه  |  يدل على أن الكذب مرات تكون ضرورية شي؟',
-    'صراحه  |  أشعر بالوحدة على الرغم من أنني تحيط بك كثيرا؟',
-    'صراحه  |  كيفية الكشف عن من يكمن عليك؟',
-    'صراحه  |  إذا حاول شخص ما أن يكرهه أن يقترب منك ويهتم بك تعطيه فرصة؟',
-    'صراحه  |  أشجع شيء حلو في حياتك؟',
-    'صراحه  |  طريقة جيدة يقنع حتى لو كانت الفكرة خاطئة" توافق؟',
-    'صراحه  |  كيف تتصرف مع من يسيئون فهمك ويأخذ على ذهنه ثم ينتظر أن يرفض؟',
-    'صراحه  |  التغيير العادي عندما يكون الشخص الذي يحبه؟',
-    'صراحه  |  المواقف الصعبة تضعف لك ولا ترفع؟',
-    'صراحه  |  نظرة و يفسد الصداقة؟',
-    'صراحه  |  ‏‏إذا أحد قالك كلام سيء بالغالب وش تكون ردة فعلك؟',
-    'صراحه  |  شخص معك بالحلوه والمُره؟',
-    'صراحه  |  ‏هل تحب إظهار حبك وتعلقك بالشخص أم ترى ذلك ضعف؟',
-    'صراحه  |  تأخذ بكلام اللي ينصحك ولا تسوي اللي تبي؟',
-    'صراحه  |  وش تتمنى الناس تعرف عليك؟',
-    'صراحه  |  ابيع المجرة عشان؟',
-    'صراحه  |  أحيانا احس ان الناس ، كمل؟',
-    'صراحه  |  مع مين ودك تنام اليوم؟',
-    'صراحه  |  صدفة العمر الحلوة هي اني؟',
-    'صراحه  |  الكُره العظيم دايم يجي بعد حُب قوي " تتفق؟',
-    'صراحه  |  صفة تحبها في نفسك؟',
-    'صراحه  |  ‏الفقر فقر العقول ليس الجيوب " ، تتفق؟',
-    'صراحه  |  تصلي صلواتك الخمس كلها؟',
-    'صراحه  |  ‏تجامل أحد على راحتك؟',
-    'صراحه  |  اشجع شيء سويتة بحياتك؟',
-    'صراحه  |  وش ناوي تسوي اليوم؟',
-    'صراحه  |  وش شعورك لما تشوف المطر؟',
-    'صراحه  |  غيرتك هاديه ولا تسوي مشاكل؟',
-    'صراحه  |  ما اكثر شي ندمن عليه؟',
-    'صراحه  |  اي الدول تتمنى ان تزورها؟',
-    'صراحه  |  متى اخر مره بكيت؟',
-    'صراحه  |  تقيم حظك ؟ من عشره؟',
-    'صراحه  |  هل تعتقد ان حظك سيئ؟',
-    'صراحه  |  شـخــص تتمنــي الإنتقــام منـــه؟',
-    'صراحه  |  كلمة تود سماعها كل يوم؟',
-    'صراحه  |  **هل تُتقن عملك أم تشعر بالممل؟',
-    'صراحه  |  هل قمت بانتحال أحد الشخصيات لتكذب على من حولك؟',
-    'صراحه  |  متى آخر مرة قمت بعمل مُشكلة كبيرة وتسببت في خسائر؟',
-    'صراحه  |  ما هو اسوأ خبر سمعته بحياتك؟',
-    '‏صراحه | هل جرحت شخص تحبه من قبل ؟',
-    'صراحه  |  ما هي العادة التي تُحب أن تبتعد عنها؟',
-    '‏صراحه | هل تحب عائلتك ام تكرههم؟',
-    '‏صراحه  |  من هو الشخص الذي يأتي في قلبك بعد الله – سبحانه وتعالى- ورسوله الكريم – صلى الله عليه وسلم؟',
-    '‏صراحه  |  هل خجلت من نفسك من قبل؟',
-    '‏صراحه  |  ما هو ا الحلم  الذي لم تستطيع ان تحققه؟',
-    '‏صراحه  |  ما هو الشخص الذي تحلم به كل ليلة؟',
-    '‏صراحه  |  هل تعرضت إلى موقف مُحرج جعلك تكره صاحبهُ؟',
-     '‏صراحه  |  هل قمت بالبكاء أمام من تُحب؟',
-    '‏صراحه  |  ماذا تختار حبيبك أم صديقك؟',
-    '‏صراحه  | هل حياتك سعيدة أم حزينة؟',
-    'صراحه  |  ما هي أجمل سنة عشتها بحياتك؟',
-    '‏صراحه  |  ما هو عمرك الحقيقي؟',
-    '‏صراحه  |  ما اكثر شي ندمن عليه؟',
-    'صراحه  |  ما هي أمنياتك المُستقبلية؟‏',
-]
-  client.on('message', message => {
-if (message.content.startsWith('^صراحه')) {
-    if(!message.channel.guild) return message.reply('** This command only for servers **');
- var client= new Discord.RichEmbed()
- .setTitle("لعبة صراحة ..")
- .setColor('RANDOM')
- .setDescription(`${Sra7a[Math.floor(Math.random() * Sra7a.length)]}`)
- .setImage("https://cdn.discordapp.com/attachments/371269161470525444/384103927060234242/125.png")
-                 .setTimestamp()
- 
-  message.channel.sendEmbed(client);
-  message.react("??")
-}
-});
-client.on('message', message => {
-     if (message.content === "^Online") {
-     let embed = new Discord.RichEmbed()
-  .setColor("#0000FF")
-  .addField("**Server: **" , client.guilds.size)
-  message.channel.sendEmbed(embed);
-    }
-
-if (message.content === '$help1') {
-              var embed  = new Discord.RichEmbed()
-                .addField("**LINKS**" ,":no_entry_sign: تم اضافه منع الروابط :no_entry_sign: ")
-                .addField("**SAY**" ,"**الاستخدام:** ``^say <الرساله>``")
-                .addField("**ID**" ,"**ايدي حقك:** ``^id``")
-                .addField("**SERVER**" ,"**معلومات عن السيرفر:** ``$server``")
-                .addField("**SUPPORT**" ,"**الاستخدام:** ``^suppport <سيرفر دعم الفنى>``")
-                .addField("**sug**" ,"**الاستخدام:** ``^Sug <اقتراح>``  ")
-                .addField("**CLEAR**" ,"**الاستخدام:** ``^clear <مسح الشات>``")
-                .addField("**sra7a**", "**الاستخدام** ``^صراحه``")
-                .addField("**Lw_5erok**", "**الاستخدام** ``^لو خيروك``")
-                .addField("**NO INVITE LINKE**","تم اضافة خاصية منع الانفيتات ")
-                .addField("**LOGIN**" , " تم اضافة خاصية التفعيل لطلب تشغيلها في السيرفر كلم المبيرمجين ")
-                .setColor('RED')
-.setColor('RED')
-  message.author.sendEmbed(embed);
-    }
-});
-client.on('message', message => {
-  if(message.content === ('^clear')) {
-  let modRole = message.guild.roles.find("name", "Admin");
-  if (!modRole) return message.reply('**ما عندك صلاحيه**'); {
-    }
-  const params = message.content.split(" ").slice(1)
-    let messagecount = parseInt(params[0]);
-    message.channel.fetchMessages({limit: messagecount})
-        .then(messages => message.channel.bulkDelete(messages));
-  }
-});
 client.on('message', message => {
     var args = message.content.split(/[ ]+/)
     if(message.content.includes('discord.gg')){
@@ -275,19 +100,14 @@ client.on('message', msg => {
   }
 });
 
-client.on('message', msg => {
-  if (msg.content === '^help1') {
-    msg.reply('**:heart:  شوف الخاص :heart:**');
-  }
-});
 
 client.on('message', msg => {
-  if (msg.content === '^help3') {
+  if (msg.content === '^help2') {
     msg.reply('**:heart: قريبا :heart:**');
   }
 });
 client.on('message', msg => {
-  if (msg.content === '^help2') {
+  if (msg.content === '^help1') {
     msg.reply('**:heart:  شوف الخاص :heart:**');
   }
 });
@@ -353,56 +173,55 @@ client.on('ready', () => {
             }
 });
   client.on("message", message => {
- if (message.content === "$help") {
+ if (message.content === "^help") {
   const embed = new Discord.RichEmbed()
       .setColor("RANDOM")
       .setFooter('─══════ {✯ArabSupport✯} ══════─')
-	  .addField('**➺ $help1 ➺**', `** ⦁ أوامر عامة ⦁ **`)
-	  .addField('**➺ $help2 ➺**', `** ⦁قائمة الاكواد ⦁ **`)
-	  .addField('**➺ $help3 ➺**', `⦁ Soon ⦁`)
+	  .addField('**➺ ^help1 ➺**', `** ⦁قائمة الاكواد ⦁ **`)
+	  .addField('**➺ ^help2 ➺**', `⦁ Soon ⦁`)
       .setFooter('─══════ {✯ArabSupport✯} ══════─')
   message.author.send({embed});
   }
 });
   client.on("message", message => {
- if (message.content === "$help2") {
+ if (message.content === "^help1") {
   const embed = new Discord.RichEmbed()
       .setColor("RANDOM")
       .setFooter('─═════════ {✯ArabSupport✯} ════════════─')
-	  .addField('**$help-js ➺** **⦁ قائمة أكواد الجافا سكربت ⦁**')
-	  .addField('**$help-soon ➺**')
+	  .addField('**^help-js ➺** **⦁ قائمة أكواد الجافا سكربت ⦁**')
+	  .addField('**^help-soon ➺**')
       .setFooter('─═════════ {✯ArabSupport✯} ════════════─')
   message.author.send({embed});
   }
 });
 
   client.on("message", message => {
- if (message.content === "$help-js") {
+ if (message.content === "^help-js") {
   const embed = new Discord.RichEmbed()
       .setColor("RANDOM")
       .setFooter('─═════════ {✯ArabSupport✯} ════════════─')
-	  .addField('**$help-js-source ➺ ⦁ قسم السورس الأساسي ⦁**')
-	  .addField('**$help-js-bc ➺ ⦁ قسم أكواد البرودكاست ⦁**')
-	  .addField('**$help-js-welcome ➺ ⦁ قسم أكواد الترحيب ⦁**')
+	  .addField('**^help-js-source ➺ ⦁ قسم السورس الأساسي ⦁**')
+	  .addField('**^help-js-bc ➺ ⦁ قسم أكواد البرودكاست ⦁**')
+	  .addField('**^help-js-welcome ➺ ⦁ قسم أكواد الترحيب ⦁**')
       .setFooter('─═════════ {✯ArabSupport✯} ════════════─')
   message.author.send({embed});
   }
 });
 
    client.on("message", message => {
- if (message.content === "$help-js-source") {
+ if (message.content === "^help-js-source") {
   const embed = new Discord.RichEmbed()
       .setColor("RANDOM")
       .setFooter('─═════════ {✯ArabSupport✯} ════════════─')
-	  .addField('**$help-js-source-1 ➺ ⦁ السورس الأساسي⦁**')
-	  .addField('**$help-js-source-2 ➺ ⦁ السورس الأساسي مع الستريمنق ومعلومات البوت ⦁**')
-	  .addField('**$help-js-source-3 ➺ ⦁ السورس الأساسي مع الستريمنق ومعلومات البوت ⦁**') 
+	  .addField('**^help-js-source-1 ➺ ⦁ السورس الأساسي⦁**')
+	  .addField('**^help-js-source-2 ➺ ⦁ السورس الأساسي مع الستريمنق ومعلومات البوت ⦁**')
+	  .addField('**^help-js-source-3 ➺ ⦁ السورس الأساسي مع الستريمنق ومعلومات البوت ⦁**') 
       .setFooter('─═════════ {✯ArabSupport✯} ════════════─')
   message.author.send({embed});
   }
 });
    client.on("message", message => {
- if (message.content === "$help-js-source-1") {
+ if (message.content === "^help-js-source-1") {
   const embed = new Discord.RichEmbed()
       .setColor("RANDOM")
       .setFooter('─═════════ {✯ArabSupport✯} ════════════─')
@@ -412,7 +231,7 @@ client.on('ready', () => {
   }
 });
    client.on("message", message => {
- if (message.content === "$help-js-source-2") {
+ if (message.content === "^help-js-source-2") {
   const embed = new Discord.RichEmbed()
       .setColor("RANDOM")
       .setFooter('─═════════ {✯ArabSupport✯} ════════════─')
@@ -421,8 +240,9 @@ client.on('ready', () => {
   message.author.send({embed});
   }
 });
+
    client.on("message", message => {
- if (message.content === "$help-js-source-3") {
+ if (message.content === "^help-js-source-3") {
   const embed = new Discord.RichEmbed()
       .setColor("RANDOM")
       .setFooter('─═════════ {✯ArabSupport✯} ════════════─')
@@ -555,5 +375,4 @@ client.on('ready', () => {
             }
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login('NDY5NDAzNjMxNjI0Mzg4NjE4.DjHNwA.6oUmTaOddidYpLZsGELc-tmP-kE');
